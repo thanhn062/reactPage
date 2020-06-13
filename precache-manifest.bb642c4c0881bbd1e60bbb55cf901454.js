5ee7bf31f9c0367a398247cfac8f8391aae09d9b
thanhn <510thanh.ngo@gmail.com>
2020-06-13 03:48:28 -0700

    Updates

---
+++ precache-manifest.bb642c4c0881bbd1e60bbb55cf901454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b1abc9595dfd5166f747afb5d6cb17",
+    "url": "/reactPage/index.html"
+  },
+  {
+    "revision": "f33f8c2066f95407f22a",
+    "url": "/reactPage/static/css/main.ccdfb3dd.chunk.css"
+  },
+  {
+    "revision": "70404e3185ca520a9e66",
+    "url": "/reactPage/static/js/2.94259b67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPage/static/js/2.94259b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33f8c2066f95407f22a",
+    "url": "/reactPage/static/js/main.1b6a8f2d.chunk.js"
+  },
+  {
+    "revision": "e2f1c9c4ae567c9b995e",
+    "url": "/reactPage/static/js/runtime-main.0586dffa.js"
+  },
+  {
+    "revision": "2a71914d80a0b761d6834f2cf112bd59",
+    "url": "/reactPage/static/media/interlaced.2a71914d.png"
+  }
+]);
